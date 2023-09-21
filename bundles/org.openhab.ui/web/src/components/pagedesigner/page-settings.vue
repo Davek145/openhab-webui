c04a4831450ba9945dd8f2698204c3db532436aa
Davek145 <123872093+Davek145@users.noreply.github.com>
2023-09-21 16:38:59 +0200

    Add editing of Tags for MainUI Pages v2
    Signed-off-by: David Kesl <davidkesl76@gmail.com>
    
    Signed-off-by: Davek145 <123872093+Davek145@users.noreply.github.com>

--- bundles/org.openhab.ui/web/src/components/pagedesigner/page-settings.vue
+++ bundles/org.openhab.ui/web/src/components/pagedesigner/page-settings.vue
@@ -1,38 +1,49 @@
 <template>
   <f7-col>
-    <f7-list inline-labels accordion-list no-hairline-md>
-      <f7-list-input label="ID" type="text" placeholder="Required" :value="page.uid" @input="page.uid = $event.target.value"
-                     :clear-button="createMode" :info="(createMode) ? 'Note: cannot be changed after the creation' : ''"
-                     required validate pattern="[A-Za-z0-9_]+" error-message="Required. A-Z,a-z,0-9,_ only" :disabled="!createMode" />
-      <f7-list-input label="Label" type="text" placeholder="Label" :value="page.config.label" @input="page.config.label = $event.target.value" required validate clear-button />
-      <f7-list-item accordion-item title="Sidebar &amp; Visibility" :disabled="page.uid === 'overview'">
-        <f7-accordion-content>
-          <f7-list-item ref="pageVisibility" title="Visible only to" smart-select :smart-select-params="{openIn: 'popover'}">
-            <select name="pagevisibility" multiple @change="updatePageVisibility">
-              <optgroup label="Roles">
-                <option value="role:administrator" :selected="isVisibleTo('role:administrator')">
-                  Administrators
-                </option>
-                <option value="role:user" :selected="isVisibleTo('role:user')">
-                  Users
-                </option>
-              </optgroup>
-            </select>
-          </f7-list-item>
-          <f7-list inline-labels no-hairline-md>
-            <f7-list-item title="Show on sidebar">
-              <f7-toggle slot="after" :checked="page.config.sidebar" @toggle:change="page.config.sidebar = $event" />
+    <f7-col>
+      <f7-list inline-labels accordion-list no-hairline-md>
+        <f7-list-input label="ID" type="text" placeholder="Required" :value="page.uid" @input="page.uid = $event.target.value"
+                      :clear-button="createMode" :info="(createMode) ? 'Note: cannot be changed after the creation' : ''"
+                      required validate pattern="[A-Za-z0-9_]+" error-message="Required. A-Z,a-z,0-9,_ only" :disabled="!createMode" />
+        <f7-list-input label="Label" type="text" placeholder="Label" :value="page.config.label" @input="page.config.label = $event.target.value" required validate clear-button />
+        <f7-list-item accordion-item title="Sidebar &amp; Visibility" :disabled="page.uid === 'overview'">
+          <f7-accordion-content>
+            <f7-list-item ref="pageVisibility" title="Visible only to" smart-select :smart-select-params="{openIn: 'popover'}">
+              <select name="pagevisibility" multiple @change="updatePageVisibility">
+                <optgroup label="Roles">
+                  <option value="role:administrator" :selected="isVisibleTo('role:administrator')">
+                    Administrators
+                  </option>
+                  <option value="role:user" :selected="isVisibleTo('role:user')">
+                    Users
+                  </option>
+                </optgroup>
+              </select>
             </f7-list-item>
-            <f7-list-input label="Sidebar order" type="number" placeholder="Assign order index to rearrange pages on sidebar" :value="page.config.order" @input="page.config.order = $event.target.value" clear-button />
-          </f7-list>
-        </f7-accordion-content>
-      </f7-list-item>
-    </f7-list>
+            <f7-list inline-labels no-hairline-md>
+              <f7-list-item title="Show on sidebar">
+                <f7-toggle slot="after" :checked="page.config.sidebar" @toggle:change="page.config.sidebar = $event" />
+              </f7-list-item>
+              <f7-list-input label="Sidebar order" type="number" placeholder="Assign order index to rearrange pages on sidebar" :value="page.config.order" @input="page.config.order = $event.target.value" clear-button />
+            </f7-list>
+          </f7-accordion-content>
+        </f7-list-item>
+      </f7-list>
+    </f7-col>
+    <f7-col v-if="page.uid != 'overview'" >
+      <f7-block-title>Non-Semantic Tags</f7-block-title>
+      <tag-input :item="page" />
+    </f7-col>
   </f7-col>
 </template>
 
 <script>
+import TagInput from '@/components/tags/tag-input.vue'
+
 export default {
+  components: {
+    TagInput
+  },
   props: ['page', 'createMode'],
   data () {
     return {}
